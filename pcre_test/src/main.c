38a010f22847b29d500d07316342fa07848bd051
Willard Lu <willard.lu@outlook.com>
2023-12-05 13:38:50 +0800

    first commit

---
+++ pcre_test/src/main.c
@@ -0,0 +1,12 @@
+// @copyright Copyright 2023 Willard Lu
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+#include <stdio.h>
+#include <pcre.h>
+
+int main() {
+  return 0;
+}

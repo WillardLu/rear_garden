c999dd7de13241a53b546dc34b7a082e5fe4d264
Willard Lu <willard.lu@outlook.com>
2023-12-06 11:11:21 +0800

    增加sqlite_c_test测试项目

---
+++ sqlite_c_test/src/main.c
@@ -0,0 +1,12 @@
+// @copyright Copyright 2023 Willard Lu
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+#include <sqlite3.h>
+#include <stdio.h>
+
+int main() {
+  return 0;
+}
